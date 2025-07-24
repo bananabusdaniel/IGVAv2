c98ed4f24dbc14d5331d865b84fffb5399414b1d
bananabusdaniel <reDblue0>
2025-07-25 02:14:13 +0700

    [trivial] is_Valid pseudocode as comment

--- profile-scraper.py
+++ profile-scraper.py
@@ -9,6 +9,23 @@ def is_valid_profile() -> bool:
     ...
     #check basic needs - create file which stores these needs in one place
     #use is_in_database function here too
+    '''
+    Requirements:
+    A. Scraped Features
+        1. Public
+        2. Under 15k followers, over 20 followers
+        3. Over 20 follwing.
+        4. Over 5 Posts
+        5. Maximum half a year since last post.
+        6. Is Woman (+human) + Hebrew used + Age over 16
+            a. Bio
+            b. Profile photo
+            c. Last 6 post photos / captions
+    B. Misc Features
+        1. Not in database already.
+
+
+    '''
 
 #Function: filter out already added profiles
 def is_in_database() -> bool:
