886ffa08ddca8ac5ffa580cb0f72858a2f11445c
bananabusdaniel <reDblue0>
2025-07-25 02:13:13 +0700

    [feat] Function for altering specific attribute in Profile object

--- db_ops.py
+++ db_ops.py
@@ -9,6 +9,18 @@ def add_profile(profile_data: dict):
     session.commit()
     session.close()
 
+#Change field of profile in database
+def alter_profile(username: str, field: str, new_value: str) -> None:
+    session = SessionLocal()
+    try:
+        profile = session.query(Profile).filter_by(username=username).one()
+        setattr(profile, field, new_value)
+        session.commit()
+    except NoResultFound:
+        print(f"Profile {username} not found.")
+    finally:
+        session.close()
+
 #Get all the profiles from database
 def get_all_profiles() -> list:
     session = SessionLocal()
