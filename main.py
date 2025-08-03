d602bc0a1c04252c881578505099be8f8485ca4f
bananabusdaniel <reDblue0>
2025-08-03 14:37:01 +0300

    [trivial] Little semantics

--- main.py
+++ main.py
@@ -3,21 +3,22 @@ from instagrapi import Client
 from instagrapi.types import User
 from utils.profile_scraper import get_source_account_followers, scrape
 
-SOURCE_ACCOUNTS = ["pilatesgirl"]
+SOURCE_ACCOUNTS = ["strongful_co"]
+profiles = []
 
 # will be depending on which one i want to log into in the future
 ACCOUNT = "alt1"
 
 # login to the account
-cl = account_login(ACCOUNT)
+cl = account_login(alt_acc=ACCOUNT)
 # list of followers
-followers = get_source_account_followers(cl, SOURCE_ACCOUNTS[0], amount=500)
+followers = get_source_account_followers(cl, SOURCE_ACCOUNTS[0], amount=5)
 
 #loop through users
 for username in followers:
     try:
         #main scraping function
-        scrape(username, cl, SOURCE_ACCOUNTS[0])
+        profiles.append(scrape(username, cl, SOURCE_ACCOUNTS[0]))
     except Exception as e:
         print(f"Invalid profile: {username}.")
         print(f"Error is {e}")
